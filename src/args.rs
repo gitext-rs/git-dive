2b6cc873da08a442e2a56d21d1959eb7ee07a3f5
Ed Page <eopage@gmail.com>
2022-09-28 15:28:19 -0500

    refactor: Upgrade to clap v4

--- src/args.rs
+++ src/args.rs
@@ -1,20 +1,18 @@
 #[derive(clap::Parser)]
-#[clap(about, author, version)]
-#[clap(
-        setting = clap::AppSettings::DeriveDisplayOrder,
-        dont_collapse_args_in_usage = true,
+#[command(about, author, version)]
+#[command(
         color = concolor_clap::color_choice(),
         allow_missing_positional = true,
     )]
-#[clap(group = clap::ArgGroup::new("mode").multiple(false).required(true))]
+#[command(group = clap::ArgGroup::new("mode").multiple(false).required(true))]
 pub struct Args {
-    #[clap(action, default_value = "HEAD")]
+    #[arg(default_value = "HEAD")]
     pub rev: String,
 
-    #[clap(required = true, action, group = "mode")]
+    #[arg(required = true, group = "mode")]
     pub file: Option<std::path::PathBuf>,
 
-    #[clap(long, value_name = "PATH", action, group = "mode")]
+    #[arg(long, value_name = "PATH", group = "mode")]
     pub dump_config: Option<std::path::PathBuf>,
 
     /// Run as if git was started in `PATH` instead of the current working directory.
@@ -28,13 +26,13 @@ pub struct Args {
     ///
     ///     git --git-dir=a.git --work-tree=b -C c status
     ///     git --git-dir=c/a.git --work-tree=c/b status
-    #[clap(short = 'C', hide = true, value_name = "PATH", parse(from_os_str))]
+    #[arg(short = 'C', hide = true, value_name = "PATH")]
     pub current_dir: Option<Vec<std::path::PathBuf>>,
 
-    #[clap(flatten)]
+    #[command(flatten)]
     pub(crate) color: concolor_clap::Color,
 
-    #[clap(flatten)]
+    #[command(flatten)]
     pub verbose: clap_verbosity_flag::Verbosity<clap_verbosity_flag::InfoLevel>,
 }
 
