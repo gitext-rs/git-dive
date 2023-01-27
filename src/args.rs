e2f0a29a3e19ece8fdc8dc6be496600368fc5734
Ed Page <eopage@gmail.com>
2023-01-26 21:07:57 -0600

    docs(cli): Mark flags as literals

--- src/args.rs
+++ src/args.rs
@@ -33,12 +33,12 @@ pub struct Args {
 
     /// Run as if git was started in `PATH` instead of the current working directory.
     ///
-    /// When multiple -C options are given, each subsequent
-    /// non-absolute -C <path> is interpreted relative to the preceding -C <path>. If <path> is present but empty, e.g.  -C "", then the
+    /// When multiple `-C` options are given, each subsequent
+    /// non-absolute `-C <path>` is interpreted relative to the preceding `-C <path>`. If `<path>` is present but empty, e.g.  `-C ""`, then the
     /// current working directory is left unchanged.
     ///
-    /// This option affects options that expect path name like --git-dir and --work-tree in that their interpretations of the path names
-    /// would be made relative to the working directory caused by the -C option. For example the following invocations are equivalent:
+    /// This option affects options that expect path name like `--git-dir` and `--work-tree` in that their interpretations of the path names
+    /// would be made relative to the working directory caused by the `-C` option. For example the following invocations are equivalent:
     ///
     ///     git --git-dir=a.git --work-tree=b -C c status
     ///     git --git-dir=c/a.git --work-tree=c/b status
