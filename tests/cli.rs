e6b045dbf43cefc3942b50b1fcdb9e2d7bacd202
Ed Page <eopage@gmail.com>
2023-03-18 10:55:11 -0500

    test: Add basic test

---
+++ tests/cli.rs
@@ -0,0 +1,37 @@
+#[test]
+fn basic() {
+    let root = snapbox::path::PathFixture::mutable_temp().unwrap();
+    let root_path = root.path().unwrap();
+    let plan = git_fixture::TodoList {
+        commands: vec![
+            git_fixture::Command::Tree(git_fixture::Tree {
+                files: [("basic.js", "test('arg1');")]
+                    .into_iter()
+                    .map(|(p, c)| (p.into(), c.into()))
+                    .collect::<std::collections::HashMap<_, _>>(),
+                message: Some("A".to_owned()),
+                author: None,
+            }),
+            git_fixture::Command::Branch("main".into()),
+        ],
+        ..Default::default()
+    };
+    plan.run(root_path).unwrap();
+
+    snapbox::cmd::Command::new(snapbox::cmd::cargo_bin!("git-dive"))
+        .arg("basic.js")
+        .current_dir(root_path)
+        .assert()
+        .success()
+        .stdout_eq(
+            "\
+HEAD 1 │ test('arg1');
+",
+        )
+        .stderr_matches(
+            "\
+",
+        );
+
+    root.close().unwrap();
+}
