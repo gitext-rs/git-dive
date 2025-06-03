c52da19090b1cf4299bc14929a59237a6dec1237
Ed Page <eopage@gmail.com>
2025-06-03 13:48:40 -0500

    style: Reduce visibility

--- src/assets/lazy_theme_set.rs
+++ src/assets/lazy_theme_set.rs
@@ -13,7 +13,7 @@ use syntect::highlighting::{Theme, ThemeSet};
 /// Same structure as a [`syntect::highlighting::ThemeSet`] but with themes
 /// stored in raw serialized form, and deserialized on demand.
 #[derive(Debug, Default, Serialize, Deserialize)]
-pub struct LazyThemeSet {
+pub(crate) struct LazyThemeSet {
     /// This is a [`BTreeMap`] because that's what [`syntect::highlighting::ThemeSet`] uses
     themes: BTreeMap<String, LazyTheme>,
 }
@@ -30,7 +30,7 @@ struct LazyTheme {
 
 impl LazyThemeSet {
     /// Lazily load the given theme
-    pub fn get(&self, name: &str) -> Option<&Theme> {
+    pub(crate) fn get(&self, name: &str) -> Option<&Theme> {
         self.themes.get(name).and_then(|lazy_theme| {
             lazy_theme
                 .deserialized
@@ -40,7 +40,7 @@ impl LazyThemeSet {
     }
 
     /// Returns the name of all themes.
-    pub fn themes(&self) -> impl Iterator<Item = &str> {
+    pub(crate) fn themes(&self) -> impl Iterator<Item = &str> {
         self.themes.keys().map(|name| name.as_ref())
     }
 }
